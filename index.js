d60f616267a071eedda67681ddc5318b5f115a13
Kah Yap <kahyap13@gmail.com>
2018-10-18 04:54:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ function happyHolidays() {
   return "Happy holidays!"
 }
 
-let happyHolidayTo = (holiday, name) => {return "Happy ${holiday}, ${name}!"}
+function happyHolidaysTo(name) {
+  return "Happy holidays, ${name}"
+}
