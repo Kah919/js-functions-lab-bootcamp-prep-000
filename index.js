8f0b4dde0dcf6283318d80cfabf1a0471dc4e535
Kah Yap <kahyap13@gmail.com>
2018-10-18 04:50:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // write your code below!
-let happyHolidays = x => return "Happy holidays!";
+function happyHolidays() {
+  return "Happy holidays"
+}
 let happyHolidaysTo = name => `Happy holidays ${name}!`;
 let happyHolidayTo = (holiday, name) => `Happy ${holiday}, ${name}!`;
 let holidayCountdown = (holiday, days) => `It's ${days} days until ${holiday}!`;
